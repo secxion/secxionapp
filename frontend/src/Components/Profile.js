04aaf811409d83faebe0d768d4612aaa9eda812f
SecxionBMXII <secxionapp@gmail.com>
2025-04-27 16:23:25 -0700

    DO

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -204,8 +204,9 @@ const Profile = () => {
                     <button
                         onClick={handleEditProfile}
                         className="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                    >
-                        <FaEdit className="mr-2" />
+                    >                        
+                        <span className="hidden sm:inline">Edit Profile</span> <FaEdit className="ml-1" />
+
                     </button>
                 </div>
 
