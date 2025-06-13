4eb8c735cd1db7114705a5b3354dcb8ebaa9f05e
SecxionBMXII <secxionapp@gmail.com>
2025-06-13 01:17:59 -0700

    c

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomain = process.env.REACT_APP_BACKEND_URL // "https://secxion.onrender.com";
+const backendDomain = process.env.REACT_APP_BACKEND_URL || "https://secxion.onrender.com";
 
 const SummaryApi = {
     signUP : {
